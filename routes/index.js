1114cffe87fa1f38ff84eae2fcf71f2867bc54f3
Anjin <hoyosf250@gmail.com>
2024-01-09 18:17:21 -0500

    First Deployment

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router()
 const messages = [
   {
     text: 'Hi there!',
-    user: 'Amando',
+    user: 'Armando',
     added: new Date()
   },
   {
@@ -17,6 +17,19 @@ const messages = [
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express', messages })
+})
+
+router.get('/new', function (req, res, next) {
+  res.render('form', { title: 'Express' })
+})
+
+router.post('/new', function (req, res, next) {
+  const messageText = req.body.text
+  const messageUser = req.body.user
+
+  messages.push({ text: messageText, user: messageUser, added: new Date() })
+
+  res.redirect('/')
 })
 
 module.exports = router
